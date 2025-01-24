d96688ec00ab4d04e2261a65773fe65583ee1419
nuyucrutt <59018677+nuyucrutt@users.noreply.github.com>
2025-01-24 23:00:33 +0700

    Updated navbar with link to view page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,10 @@ const Navbar: React.FC = () => {
     return (
         <nav className="bg-white shadow-md">
             <ul className="flex space-x-4">
-                <li><Link className="text-gray-800" href="/">Serch</Link></li>
+                <li><Link className="text-gray-800" href="/">Home</Link></li>
+                <li><Link className="text-gray-800" href="/admin">Admin</Link></li>
+                <li><Link className="text-gray-800" href="/user">User</Link></li>
+                <li><Link className="text-gray-800" href="/view">View</Link></li>
             </ul>
         </nav>
     );
