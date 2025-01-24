4be69f41cd2cd101829b582c3ecd17c14db94950
nuyucrutt <59018677+nuyucrutt@users.noreply.github.com>
2025-01-24 19:00:32 +0700

    Initial commit

---
+++ src/components/AdminView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminView: React.FC = () => {
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+            {/* Admin functionalities will be implemented here */}
+        </div>
+    );
+};
+
+export default AdminView;
---
+++ src/components/UserView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserView: React.FC = () => {
+    return (
+        <div>
+            <h1>User Dashboard</h1>
+            {/* User functionalities will be implemented here */}
+        </div>
+    );
+};
+
+export default UserView;
